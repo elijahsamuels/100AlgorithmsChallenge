49fa8eb595faaa100d167d8a9709ecb5283d9c0c
Elijah Samuels <elijahsamuels@gmail.com>
2021-05-17 21:11:22 -0600

    sovled

--- absoluteValuesSumMinization/absoluteValuesSumMinization.ts
+++ absoluteValuesSumMinization/absoluteValuesSumMinization.ts
@@ -1,8 +1,14 @@
 export function absoluteValuesSumMinimization(a: number[]): number {
-    return 5;
+    
+    let isEven = a.length % 2 === 0;
+
+    return isEven ? a[a.length / 2 - 1] : a[Math.floor(a.length / 2)];
+
+
+
 }
 
-// console.log(absoluteValuesSumMinimization([2, 4, 7]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
+console.log(absoluteValuesSumMinimization([2, 4, 7]), 4);
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6]), 4);
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]), 7);
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]), 7);
